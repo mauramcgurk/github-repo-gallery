b104278bc02b0a7e5d1d7d66871931f6a40e8cd5
Maura McGurk <maura@mauramcgurk.com>
2022-12-29 22:16:05 -0800

    Added empty array for languages, created event listener and function to display details of individual repos

--- js/script.js
+++ js/script.js
@@ -7,22 +7,24 @@ const individualRepoData = document.querySelector(".repo-data");
 const getProfileData = async function () {
     const request = await fetch (`https://api.github.com/users/${username}`);
     const profileData = await request.json();
-    console.log(profileData);
+    //console.log(profileData);
     displayProfile(profileData); //
 };
 
 const displayProfile = function (profileData) { //keep it outside so it's available for all. Don't pass request as paramaeter bc it has too much extra info. Json data is what we need. 
     const userInfo = document.createElement("user-info");
-    userInfo.innerHTML = `<figure>
-    <img alt="user avatar" src=${profileData.avatar_url} /> 
-  </figure>
-  <div>
-    <p><strong>Name:</strong> ${profileData.name}</p>
-    <p><strong>Bio:</strong> ${profileData.bio}</p>
-    <p><strong>Location:</strong> ${profileData.location}</p>
-    <p><strong>Number of public repos:</strong> ${profileData.public_repos}</p>
-  </div>` //thi is all in a string bc need to embed within $ later and string is easiest way
-    profileOverview.appendChild(userInfo); //adding as a child. InnerHTML is kind of doing the same thing.
+    userInfo.innerHTML = `
+      <figure>
+        <img alt="user avatar" src=${profileData.avatar_url} /> 
+      </figure>
+      <div>
+        <p><strong>Name:</strong> ${profileData.name}</p>
+        <p><strong>Bio:</strong> ${profileData.bio}</p>
+        <p><strong>Location:</strong> ${profileData.location}</p>
+        <p><strong>Number of public repos:</strong> ${profileData.public_repos}</p>
+      </div>` //this is all in a string bc need to embed within $ later and string is easiest way
+    
+   profileOverview.appendChild(userInfo); //adding as a child. InnerHTML is kind of doing the same thing.
 
     getRepos();
   };
@@ -33,8 +35,6 @@ const getRepos = async function () {
     console.log(repos);
     displayRepoNames(repos);
 };
-
-getRepos();
 
 getProfileData(); //calling function - if you don't call it nothing will happen. That's why you call outside function. Making it isn't enough.
 
@@ -62,15 +62,33 @@ const getRepoData = async function (repoName) {
   //displayProfile(profileData); //I erased this by accident and my profile pic disappeared! Then I added back and it was "Undefined"?
   const fetchLanguages = await fetch (repoInfo.languages_url); // ask Arnav / I'm not sure what needs to happen here to use language property?
   const languageData = await fetchLanguages.json();
-  console.log(languageData);
+  //console.log(languageData);
+  const languages = []; //empty array is for adding to
+  for (const language in languages) { //Am I confusing var names here?
+    console.log(language);
+    languages.push(language);
+  }
+
+  displayRepoData(repoInfo, languages);
 };
 
 
 const displayRepoData = async function (repoInfo, languages) {
+  individualRepoData.innerHTML = "";
+  individualRepoData.classList.remove ("hide");
+  repoBlock.classList.add ("hide"); //Is something going wrong here - after clicking on one, I can only see that one; the other 17 disappear unless I refresh page
+ 
+  const div = document.createElement("div"); 
   
+  div.innerHTML = 
+  `<h3>Name: ${repoInfo.name}</h3>
+  <p>Description: ${repoInfo.description}</p>
+  <p>Default Branch: ${repoInfo.default_branch}</p>
+  <p>Languages: ${languages.join(", ")}</p>
+  <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`
+
+  individualRepoData.append(div);
 };
 
-displayRepoData ();
-
-//my repos are displaying twice, for some reason? 12/10/22
+//displayRepoData ();
 
